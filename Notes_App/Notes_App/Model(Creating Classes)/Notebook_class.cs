1a03b477c611f015a50818e005f66f9e2bc4717e
ctelotuong <56626638+ctelotuong@users.noreply.github.com>
2019-12-06 09:43:17 +0700

    Create Notebook_class.cs

---
+++ Notes_App/Notes_App/Model(Creating Classes)/Notebook_class.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Notes_App.Model_Creating_Classes_
+{
+    class Notebook_class : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private int id_user;
+        public int Id_user
+        {
+            get { return id_user; }
+            set 
+            { 
+                id_user = value;
+                OnPropertyChanged("Id_user");            
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
