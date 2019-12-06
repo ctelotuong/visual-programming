f939c1536e8853fa72a274884f88689fd4ef9254
ctelotuong <56626638+ctelotuong@users.noreply.github.com>
2019-12-06 10:57:15 +0700

    Some change
    change in View and View_Model

---
+++ Notes_App/Notes_App/ViewModel/DatabaseHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;//important(Tuong)
+
+namespace Notes_App.ViewModel
+{
+    public class DatabaseHelper
+    {
+        //dbFile will save in current Directory of the sortware so you can easily will it(Tuong)
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "noteDB.db3");
+        //using static so we can easily access it without having create an instance of the database helper class (Tuong)
+        //<T> is generic method
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+            using (SQLiteConnection conn= new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int numberofRows=conn.Insert(item);
+                if(numberofRows >0)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+        public static bool Update<T>(T item)
+        {
+            bool result = false;
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int numberofRows = conn.Update(item);
+                if (numberofRows > 0)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+        public static bool Detele<T>(T item)
+        {
+            bool result = false;
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int numberofRows = conn.Delete(item);
+                if (numberofRows > 0)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Notes_App/Notes_App/ViewModel/Login_ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notes_App.Model_Creating_Classes_;
+
+namespace Notes_App.ViewModel
+{
+   public class Login_ViewModel
+    {
+        private User_HomePage user;
+        public User_HomePage User
+        {
+            get
+            {
+                return user;
+            }
+            set
+            {
+                user = value;
+            }
+        }
+
+    }
+}
